74591f6de1dc55db1760e028e36d7607c7d708db
tousif101 <tousif101@gmail.com>
2022-12-27 01:25:22 -0500

    inital commit

---
+++ testGPT.py
@@ -0,0 +1,28 @@
+import openai
+import twilio
+from twilio.twiml.messaging_response import MessagingResponse
+from flask import Flask, request, redirect
+
+# Set up the OpenAI API client
+openai.api_key = openai_api_key
+
+
+# Set up a "webhook" to listen for incoming SMS messages
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_webhook():
+    resp = MessagingResponse()
+
+    # Get the sender and body of the message
+    sender = request.form["From"]
+    body = request.form["Body"]
+
+    response = openai.Completion.create(engine="text-davinci-002", prompt=body, max_tokens=1024)
+    response_text = response["choices"][0]["text"]
+
+    # Add a message
+    resp.message(response_text)
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
