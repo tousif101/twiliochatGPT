9eae3b72fd5246bec8c5a2a47c980c9b40033c23
tousif101 <tousif101@gmail.com>
2022-12-28 23:33:20 -0500

    try to use the message client

--- app.py
+++ app.py
@@ -2,13 +2,29 @@ import openai
 import twilio
 from twilio.twiml.messaging_response import MessagingResponse
 from flask import Flask, request, redirect
+from twilio.rest import Client
+
 import os
 
 app = Flask(__name__)
 
 openai_api_key=os.environ.get('OPENAIKEY')
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+
 # Set up the OpenAI API client
 openai.api_key = openai_api_key
+
+client = Client(account_sid, auth_token)
+
+
+
+'''
+TODO: 
+    - Add authentication for twilio phone numbers 
+    - Add state for can hold conversations 
+
+'''
 
 # Set up a "webhook" to listen for incoming SMS messages
 @app.route("/sms", methods=['GET', 'POST'])
@@ -26,7 +42,13 @@ def sms_webhook():
     print(sender)
     resp.message(to=sender,body=response_text, from_='+18304653338')
 
+    message = client.messages.create(
+                              body=response_text,
+                              from_='+18304653338',
+                              to=sender
+                          )
+
     return str(resp)
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True)
